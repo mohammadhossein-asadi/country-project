8286868992d0533f822b6c84e07931cf7e7f2946
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-06-02 00:13:55 +0430

    commit

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,20 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import CountryCard from "./CountryCard";
 
 const Homepage = () => {
   const [countryData, setCountryData] = useState();
+
+  // URL
+  const url = "https://restcountries.com/v3.1/all";
+
+  // Fetch data
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setCountryData(res.data);
+      console.log(res.data);
+    });
+  }, []);
 
   return <div></div>;
 };
